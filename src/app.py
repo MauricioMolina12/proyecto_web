9e9cd552434df70c099251e8e170db623dff0f6f
Mauricio Molina <maupena9@gmail.com>
2023-09-28 13:33:41 -0500

    firts commit

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, request, jsonify, json, session, render_template
+from.config.bd import app, db
+
+@app.route("/")
+def index():
+    
+    return "Hola mundo"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/config/bd.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/proyecto_cicla'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secret_key = "proyectobici"
+
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
