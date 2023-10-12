09f7b89d8e4c61e73d3bcd68ca59e234c8eb06dc
Mauricio Molina <maupena9@gmail.com>
2023-10-12 13:15:47 -0500

    adding communities

--- src/config/static/JS/login.js
+++ src/config/static/JS/login.js
@@ -1,29 +1,3 @@
 document.getElementById("RegisterGO").addEventListener("click", function() {
     window.location.href = "/register";
 });
-
- const usuarioInput = document.querySelector('input[name="user"]');
- const emailInput = document.querySelector('input[name="email"]');
- const passwordInput = document.querySelector('input[name="password"]');
- const confirmPasswordInput = document.querySelector('input[name="Cpassword"]');
-
- const registroBtn = document.querySelector('.btn');
-
- registroBtn.addEventListener('click', () => {
-     const nombre = usuarioInput.value;
-     const correo = emailInput.value;
-     const contraseña = passwordInput.value;
-     const confirmarContraseña = confirmPasswordInput.value;
-
-     axios.post('/saveusuario', {
-         nombre: nombre,
-         correo: correo,
-         contraseña: contraseña
-     })
-     .then(function (response) {
-         console.log(response.data);
-     })
-     .catch(function (error) {
-         console.error(error);
-     });
- });
--- src/config/static/JS/script.js
+++ src/config/static/JS/script.js
@@ -2,4 +2,40 @@ document.getElementById("loginGO").addEventListener("click", function() {
     window.location.href = "/login";
 });
 
+const usuarioInput = document.querySelector('input[name="user"]');
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
+const confirmPasswordInput = document.querySelector('input[name="Cpassword"]');
+
+const registroBtn = document.querySelector('.btn');
+
+registroBtn.addEventListener('click', () => {
+    const nombre = usuarioInput.value;
+    const correo = emailInput.value;
+    const contraseña = passwordInput.value;
+    const confirmarContraseña = confirmPasswordInput.value;
+
+    if (!nombre || !correo || !contraseña || !confirmarContraseña) {
+        alert('No ha completado la información en los campos.');
+        return;
+    }
+
+    if (contraseña !== confirmarContraseña) {
+        alert('Las contraseñas no coinciden.');
+        return;
+    }
+
+    axios.post('http://localhost:5000/api/saveusuario', {
+        nombre: nombre,
+        correo: correo,
+        contraseña: contraseña
+    })
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.error(error);
+        alert('Error al registrar usuario.');
+    });
+});
 
